04832293bc4f10e23bc4c2f7ffb920b880329605
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-18 18:50:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,19 +1,3 @@
 def map_to_negativize(array)
-  array.map{|n| -n}
-end
-
-def map_to_no_change(array)
-  array.map{|n| n}
-end
-
-def map_to_double(array)
-  array.map{|n| n * 2}
-end
-
-def map_to_square(array)
-  array.map{|n| n ** 2}
-end
-
-def reduce_to_total(array, starting_point)
-  array.reduce{ |total, n| total + n}
+  array.map(|n| -n)
 end
